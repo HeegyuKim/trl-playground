ee157f01e5fb417acf202881d3bac10dab363a61
HeegyuKim <heekue83>
2023-08-25 22:59:08 +0900

    생성코드 추가

---
+++ generate_sentiment_sample.py
@@ -0,0 +1,44 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+
+from transformers import pipeline
+import jsonlines
+from tqdm import tqdm
+import torch
+
+
+prompts = [
+    "please write a positive restaurant review",
+    "please write a negative restaurant review",
+]
+output = "sentimental_review_baseline.jsonl"
+model_id = "google/flan-t5-base"
+model = pipeline("text2text-generation", model_id, device=0 if torch.cuda.is_available() else "cpu")
+
+generation_kwargs = {
+    "top_k": 0.0,
+    "top_p": 1.0,
+    "num_return_sequences": 8,
+    "max_new_tokens": 128,
+    "min_length": 5,
+    "do_sample": True,
+    "early_stopping": True
+}
+
+num_gens = 10000
+
+fout = jsonlines.open(output, "w")
+prompt_iter_count = num_gens // generation_kwargs["num_return_sequences"]
+tqdm = tqdm(range(prompt_iter_count * len(prompts)))
+
+for prompt in prompts:
+    for _ in range(prompt_iter_count):
+        outputs = model(prompt, **generation_kwargs)
+
+        for item in outputs:
+            item["prompt"] = prompt
+            fout.write(item)
+        
+        tqdm.update(1)
+
+fout.close()
